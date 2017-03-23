30ad0346444fb66ea4d6c8fea3426a7661177d55
Luke De Feo <luke.defeo@thetrainline.com>
2017-03-23 18:20:43 +0000

    removed wierd t

--- personal/clojure.el
+++ personal/clojure.el
@@ -5,7 +5,7 @@
 ;;
 (prelude-require-packages '(paredit))
 (autoload 'enable-paredit-mode "paredit"
-  "Turn on pseudo-structural editing of Lisp code." t)
+  "Turn on pseudo-structural editing of Lisp code.")
 (add-hook 'emacs-lisp-mode-hook       #'enable-paredit-mode)
 (add-hook 'eval-expression-minibuffer-setup-hook #'enable-paredit-mode)
 (add-hook 'ielm-mode-hook             #'enable-paredit-mode)
